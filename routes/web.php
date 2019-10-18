cd6293efb216b95225de3de56792a41cc76646db
Ivy Lin <a0981792089@gmail.com>
2019-10-18 19:41:40 +0800

    第二個路由：新增任務與資料驗證

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,23 @@ Route::get('/', function () {
 });
 // 增加新的任務
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
 });
+
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
     //
